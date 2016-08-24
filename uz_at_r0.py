da58a5318b0b4e78f259884465fa2822c9961270
phsunrise <phsun@dhcp-swlan-public-82-43.slac.stanford.edu>
2016-08-23 21:45:00 -0700

    plotting both samples

--- uz_at_r0.py
+++ uz_at_r0.py
@@ -2,19 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
 
-sample = 'Al'
-datadir = "/afs/slac.stanford.edu/u/xo/phsun/data1/"
-sdata = np.load(datadir+"%s_s_0.npy"%sample)
-rank = 1
-while os.path.isfile(datadir+"%s_s_%d.npy"%(sample, rank)):
-    sdata = np.vstack((sdata, datadir+"%s_s_%d.npy"%(sample, rank)))
-    rank += 1
+colors = ['r', 'b']
+datadir = "data/"
+for i_sample, sample in enumerate(['Al', 'Cu']):
+    sdata = np.load(datadir+"%s_s_0.npy"%sample)
+    rank = 1
+    while os.path.isfile(datadir+"%s_s_%d.npy"%(sample, rank)):
+        sdata = np.vstack((sdata, \
+                 np.load(datadir+"%s_s_%d.npy"%(sample, rank))))
+        rank += 1
 
-sdata_r0 = []
-for i in xrange(len(sdata)):
-    if sdata[i, 0] == 0.:
-        sdata_r0.append(sdata[i])
-sdata_r0 = np.array(sdata_r0)
+    sdata_r0 = []
+    for i in xrange(len(sdata)):
+        if sdata[i, 0] == 0.:
+            sdata_r0.append(sdata[i])
+    sdata_r0 = np.array(sdata_r0)
 
-plt.scatter(sdata_r0[:,2], sdata_r0[:,5])
-plt.show()
+    plt.scatter(sdata_r0[:,2], sdata_r0[:,5], c=colors[i_sample], \
+            label=sample)
+
+plt.legend()
+plt.gca().invert_yaxis()
+plt.savefig("/Users/phsun/Dropbox/uz_r0.pdf")
