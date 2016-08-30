7694ab9d4b9bcbc261d6d1270e3a7f3d61c7f20b
phsunrise <phsun@dhcp-swlan-public-82-43.slac.stanford.edu>
2016-08-29 17:25:39 -0700

    still working on it!

--- atoms_s.py
+++ atoms_s.py
@@ -35,9 +35,9 @@ a1 = a0*np.sqrt(2)/2.
 a2 = a0*np.sqrt(3)/3.
 ex_p_th = np.pi/6.
 ey_p_th = np.pi/2.
-ex_p = a1*(ex*np.cos(ex_p_th)+ey*np.sin(ex_p_th))
-ey_p = a1*(ex*np.cos(ey_p_th)+ey*np.sin(ey_p_th))
-ez_p = a2*ez
+ex_p = a1*np.array([np.cos(ex_p_th), np.sin(ex_p_th), 0.])
+ey_p = a1*np.array([np.cos(ey_p_th), np.sin(ey_p_th), 0.])
+ez_p = a2*np.array([0., 0., 1.])
 ## From now on, the coordinates will be in units of ex_p, ey_p, ez_p
 '''
 Here we assume that the dislocation loop has the configuration of plane A
@@ -49,12 +49,44 @@ orig_A = np.array([0., 0., 1.5])
 orig_B = np.array([1./3, 1./3, 2.5])
 orig_C = np.array([-1./3, 2./3, 0.5])
 
-rhomax = 5.*R
-zmax = 10.*R
 ## the summation goes to rho = 5R in xy plane, and z from -10R to 10R
 ##     (both roughly, since the origins will give some offset)
+rhomax = 5.*R
+zmax = 10.*R
 for zind in np.arange(np.ceil(-zmax/(3.*a2)), np.floor(zmax/(3.*a2))):
+    if zind == 0.:
+        continue
     _xlim = np.floor(rhomax / a1 * 2./np.sqrt(3))
     for xind in np.arange(-_xlim, _xlim+1):
         _ylims = np.roots([1., x, x**2-(rhomax/a1)**2])
         for yind in np.arange(np.ceil(np.min(_ylims)), np.floor(np.max(_ylims))+1):
+            x, y, z = (xind*ex_p + yind*ey_p + zind*ez_p)/R
+
+            n = 30
+            n1 = n
+            integrand = 0.
+            for jSum1, th1 in enumerate(np.linspace(0., 2.*np.pi, n1, endpoint=False)):
+                coeff = (2 if jSum1%2==0 else 4)*2*np.pi/n1/3
+                        # coefficient for th1 integration
+                a = -(x*np.cos(th1)+y*np.sin(th1)+1.)/z
+                b = -(x*np.cos(th1)+y*np.sin(th1)-1.)/z
+
+                for jSum in np.arange(1, n+1):
+                    t = (b+a)/2.+(b-a)*np.cos((2.*jSum-1)*
+                                            np.pi/2./n)/2.
+                    e = (np.cos(th1)*ex+np.sin(th1)*ey+t*ez)/(1.+t**2)**0.5
+                    eloop = np.array([np.cos(th1),np.sin(th1),t])/(1.+t**2)**0.5
+                    g = np.zeros((3,3))
+                    sum1 = sum(e**2/(C44+d*e**2))
+                    for i in xrange(3):
+                        for j in xrange(3):
+                            g[i,j] = ((1 if i==j else 0)/(C44+d*e[i]**2) - \
+                                (e[i]*e[j]/(C44+d*e[i]**2)/(C44+d*e[j]**2)*\
+                                 (C44+C12)/(1.+(C44+C12)*sum1)
+                                )
+                              )
+                    gloop =  np.einsum('ij,kl,jl', rot, rot, g)
+                    integrand += (coeff*eloop.dot(np.array([x,y,z]))*\
+                        np.einsum('ij,i,jk', Ploop, eloop, gloop))
+            s = -1./(4.*np.pi**2*R**2*n*abs(z))*integrand
+     
