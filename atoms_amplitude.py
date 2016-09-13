eae71c250ce9aff5826c4cedf5b48963f24f9c20
Peihao Sun <phsun@pslogin7a.slac.stanford.edu>
2016-09-12 22:27:32 -0700

    not tiered

--- atoms_amplitude.py
+++ atoms_amplitude.py
@@ -11,9 +11,10 @@ elif sample == 'Cu':
 elif sample == 'W':
     from W_parameters import *
 
+datadir = "%s_R%d/" % (sample, R)
 q_array = np.linspace(-0.5, 0.5, 101)
 
-D = 2.0*R ## parameter in gaussian used to smooth out the boundary
+D = 4.0*R ## parameter in gaussian used to smooth out the boundary
 
 if __name__ == '__main__':
     from getopt import getopt
@@ -24,21 +25,21 @@ if __name__ == '__main__':
         if o == '-d':
             do_debug = True
 
-    _list = [(looptype, i_ori, ori) for looptype in looptypes for i_ori, ori in enumerate(orientations)]
-    filelists = []
-    for looptype, i_ori, ori in _list:
-        # first get list of unprocessed data
-        filelists.append([])
-        for i_file in xrange(NFILES_max):
-            if not os.path.isfile("data/%s_atoms_s_%s_R%d_%04d.npy"%(\
-                  sample, looptype, R, i_file)):
-                  break
+    _list = [(looptype, tier, i_ori, ori) for looptype in looptypes for tier in range(1, MAXTIER+1) 
+                            for i_ori, ori in enumerate(orientations)]
+    # first get list of unprocessed data
+    filelist = []
+    for looptype, tier, i_ori, ori in _list:
+        i_file = 0
+        while os.path.isfile(datadir+"%s_atoms_s_%s_T%d_R%d_%04d.npy"%(\
+                  sample, looptype, tier, R, i_file)):
             if not os.path.isfile(\
-                  "data/%s_atoms_amplitude_%s_R%d_ori%d_%04d.npy"%(\
-                    sample, looptype, R, i_ori, i_file)):
-                filelists[-1].append(i_file)
+                  "data/%s_atoms_amplitude_%s_T%d_R%d_ori%d_%04d.npy"%(\
+                    sample, looptype, tier, R, i_ori, i_file)):
+                filelist.append([looptype, tier, i_ori, ori, i_file])
+            i_file += 1
     if do_debug:
-        print filelists
+        print filelist
         sys.exit(0)
 
     from mpi4py import MPI
@@ -46,59 +47,57 @@ if __name__ == '__main__':
     rank = comm.Get_rank()
     nprocs = comm.Get_size()
 
-    for (looptype, i_ori, ori), filelist in zip(_list, filelists):
-        print "Processing looptype %s, orientation %d" % (looptype, i_ori)
-        for i_i_file, i_file in enumerate(filelist):
-            ## here is the parallelism criterion
-            if i_i_file % nprocs != rank:
-                continue
+    for i_i_file, [looptype, tier, i_ori, ori, i_file] in enumerate(filelist):
+        ## here is the parallelism criterion
+        if i_i_file % nprocs != rank:
+            continue
 
-            ## read s data
-            sdata = np.load("data/%s_atoms_s_%s_R%d_%04d.npy"%(sample, looptype, R, i_file))
-            if looptype == 'vac':
-                pass
-            elif looptype == 'int':
-                sdata[:, 3:6] = -sdata[:, 3:6]
+        ## read s data
+        sdata = np.load("data/%s_atoms_s_%s_T%d_R%d_%04d.npy"%(sample, looptype, tier, R, i_file))
+        if looptype == 'vac':
+            pass
+        elif looptype == 'int':
+            sdata[:, 3:6] = -sdata[:, 3:6]
 
-            amplitudes = [] 
-            amplitudes1 = []  # amplitude inside rho<5R, |z|<10R for comparison
-            amplitudes2 = []  # amplitude inside rho<10R, |z|<20R for comparison
-            for i_qval, qval in enumerate(q_array):
-                q = qabs * np.einsum('ij,j', ori, eq)
-                qloop = np.einsum('ij,j', rot, q)
-                K = np.einsum('ij,j', ori, h) + q
-                Kloop = np.einsum('ij,j', rot, K)
+        amplitudes = [] 
+        #amplitudes1 = []  # amplitude inside rho<5R, |z|<10R for comparison
+        #amplitudes2 = []  # amplitude inside rho<10R, |z|<20R for comparison
+        for i_qval, qval in enumerate(q_array):
+            q = qabs * np.einsum('ij,j', ori, eq)
+            qloop = np.einsum('ij,j', rot, q)
+            K = np.einsum('ij,j', ori, h) + q
+            Kloop = np.einsum('ij,j', rot, K)
 
-                amplitude = 0.
-                amplitude1 = 0.
-                amplitude2 = 0.
-                ''' 
-                no laue term calculation in this script; this is taken care of 
-                in the intensity script
-                '''
-                for line in sdata:
-                    r = line[0:3]
-                    s = line[3:6]
-                    r1 = r+s
-                    qr = qloop.dot(r)
-                    Ks = Kloop.dot(s)
+            amplitude = 0.
+            #amplitude1 = 0.
+            #amplitude2 = 0.
+            ''' 
+            no laue term or form factor calculation in this script; these are taken care of 
+            in the intensity script
+            '''
+            for line in sdata:
+                r = line[0:3]
+                s = line[3:6]
+                r1 = r+s
+                qr = qloop.dot(r)
+                Ks = Kloop.dot(s)
 
-                    _temp = (np.cos(qr)*(np.cos(Ks)-1.)-\
-                             np.sin(qr)*np.sin(Ks))*np.exp(-0.5*r1.dot(r1)/D**2)
-                    amplitude += _temp
-                    if np.linalg.norm(r[0:2])<=50. and abs(r[2])<=100.:
-                        amplitude1 += _temp
-                    if np.linalg.norm(r[0:2])<=100. and abs(r[2])<=200.:
-                        amplitude2 += _temp
+                _temp = (np.cos(qr)*(np.cos(Ks)-1.)-\
+                         np.sin(qr)*np.sin(Ks))*np.exp(-0.5*r1.dot(r1)/D**2)
+                amplitude += _temp
+                #if np.linalg.norm(r[0:2])<=50. and abs(r[2])<=100.:
+                #    amplitude1 += _temp
+                #if np.linalg.norm(r[0:2])<=100. and abs(r[2])<=200.:
+                #    amplitude2 += _temp
 
-                amplitudes.append([qval, amplitude])
-                amplitudes1.append([qval, amplitude1])
-                amplitudes2.append([qval, amplitude2])
+            amplitudes.append([qval, amplitude])
+            #amplitudes1.append([qval, amplitude1])
+            #amplitudes2.append([qval, amplitude2])
 
-            # save data
-            np.save("data/%s_atoms_amplitude_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file), amplitudes)
-            np.save("data/%s_atoms_amplitude1_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file), amplitudes1)
-            np.save("data/%s_atoms_amplitude2_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file), amplitudes2)
+        # save data
+        np.save("data/%s_atoms_amplitude_%s_T%d_R%d_ori%d_%04d.npy"%(\
+                            sample, looptype, tier, R, i_ori, i_file), amplitudes)
+        #np.save("data/%s_atoms_amplitude1_%s_T%d_R%d_ori%d_%04d.npy"%(\
+        #                    sample, looptype, tier, R, i_ori, i_file), amplitudes1)
+        #np.save("data/%s_atoms_amplitude2_%s_T%d_R%d_ori%d_%04d.npy"%(\
+        #                    sample, looptype, tier, R, i_ori, i_file), amplitudes2)
--- combine_amplitudes.py
+++ combine_amplitudes.py
@@ -1,7 +1,6 @@
 import numpy as np
 import sys, os
-
-data_dir = "W_R10_D_3.0R/"
+from info import *
 
 sample = 'W'
 looptypes = ['int', 'vac']
@@ -11,31 +10,35 @@ elif sample == 'Cu':
     from Cu_parameters import *
 elif sample == 'W':
     from W_parameters import *
+data_dir = "%s_R%d/" % (sample, R)
 
-_list = [(looptype, i_ori, ori) for looptype in looptypes for i_ori, ori in enumerate(orientations)]
-for looptype, i_ori, ori in _list:
-    amplitudes = np.load(data_dir+"%s_atoms_amplitude_%s_R%d_ori%d_0000.npy"%(\
-                                sample, looptype, R, i_ori))
-    amplitudes1 = np.load(data_dir+"%s_atoms_amplitude1_%s_R%d_ori%d_0000.npy"%(\
-                                sample, looptype, R, i_ori))
-    amplitudes2 = np.load(data_dir+"%s_atoms_amplitude2_%s_R%d_ori%d_0000.npy"%(\
-                                sample, looptype, R, i_ori))
+
+_list = [(looptype, tier, i_ori, ori) for looptype in looptypes for tier in range(1, MAXTIER+1) \
+            for i_ori, ori in enumerate(orientations)]
+for looptype, tier, i_ori, ori in _list:
+    amplitudes = np.load(data_dir+"%s_atoms_amplitude_%s_T%d_R%d_ori%d_0000.npy"%(\
+                                sample, looptype, tier, R, i_ori))
+    #amplitudes1 = np.load(data_dir+"%s_atoms_amplitude1_%s_R%d_ori%d_0000.npy"%(\
+    #                            sample, looptype, R, i_ori))
+    #amplitudes2 = np.load(data_dir+"%s_atoms_amplitude2_%s_R%d_ori%d_0000.npy"%(\
+    #                            sample, looptype, R, i_ori))
     i_file = 1 
     while True:
         try:
-            amplitudes[:,1] += np.load(data_dir+"%s_atoms_amplitude_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file))[:,1]
-            amplitudes1[:,1] += np.load(data_dir+"%s_atoms_amplitude1_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file))[:,1]
-            amplitudes2[:,1] += np.load(data_dir+"%s_atoms_amplitude2_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file))[:,1]
+            amplitudes[:,1] += np.load(data_dir+"%s_atoms_amplitude_%s_T%d_R%d_ori%d_%04d.npy"%(\
+                                sample, looptype, tier, R, i_ori, i_file))[:,1]
+            #amplitudes1[:,1] += np.load(data_dir+"%s_atoms_amplitude1_%s_R%d_ori%d_%04d.npy"%(\
+            #                    sample, looptype, R, i_ori, i_file))[:,1]
+            #amplitudes2[:,1] += np.load(data_dir+"%s_atoms_amplitude2_%s_R%d_ori%d_%04d.npy"%(\
+            #                    sample, looptype, R, i_ori, i_file))[:,1]
             i_file += 1
         except IOError:
             break
 
-    np.save(data_dir+"%s_atoms_amplitude_%s_R%d_ori%d_combined.npy"%(\
-                        sample, looptype, R, i_ori), amplitudes)
-    np.save(data_dir+"%s_atoms_amplitude1_%s_R%d_ori%d_combined.npy"%(\
-                        sample, looptype, R, i_ori), amplitudes1)
-    np.save(data_dir+"%s_atoms_amplitude2_%s_R%d_ori%d_combined.npy"%(\
-                        sample, looptype, R, i_ori), amplitudes2)
+    np.save(data_dir+"%s_atoms_amplitude_%s_T%d_R%d_ori%d_combined.npy"%(\
+                        sample, looptype, tier, R, i_ori), amplitudes)
+    #np.save(data_dir+"%s_atoms_amplitude1_%s_R%d_ori%d_combined.npy"%(\
+    #                    sample, looptype, R, i_ori), amplitudes1)
+    #np.save(data_dir+"%s_atoms_amplitude2_%s_R%d_ori%d_combined.npy"%(\
+    #                    sample, looptype, R, i_ori), amplitudes2)
+    print "done %s, tier %d, orientation %d" % (looptype, tier, i_ori)
