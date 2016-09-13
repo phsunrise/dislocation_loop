115b7b265ff10c1933d03a5dd11521bca91415e6
Peihao Sun <phsun@pslogin7a.slac.stanford.edu>
2016-09-12 23:18:29 -0700

    tiered

--- atoms_intensity.py
+++ atoms_intensity.py
@@ -3,6 +3,7 @@ import sys, os
 import matplotlib.pyplot as plt
 from info import *
 from atoms_amplitude import *
+from formfactor import formfactor
 
 print "sample:", sample
 print "R=%.1f, D=%.1f*R"%(R, D/R)
@@ -14,72 +15,62 @@ ax = fig.add_subplot(1, 1, 1)
 for looptype in looptypes:
     print "starting looptype %s..." % looptype
     intensities = [] 
-    intensities1 = []
-    intensities2 = []
+    #intensities1 = []
+    #intensities2 = []
     for i_ori, ori in enumerate(orientations):
         print "starting orientation %d..." % i_ori
         amplitudes = [] 
-        ## first calculate the laue term
+        ff_tier2 = []
+        ff_tier3 = []
         for i_qval, qval in enumerate(q_array):
             q = qval * np.einsum('ij,j', ori, eq)
             qloop = np.einsum('ij,j', rot, q)
             K = np.einsum('ij,j', ori, h) + q
             Kloop = np.einsum('ij,j', rot, K)
+            
+            ## get the form factor
+            ff_tier2.append(formfactor(qloop, 2))
+            ff_tier3.append(formfactor(qloop, 3))
 
             amplitude = 0.
             '''
-            The dislocation loop is inside the region r <= R, which
+            first calculate the laue term
+            The dislocation loop is inside the region r <= R
             '''
-            _xlim = np.floor(R / a1 * 2./np.sqrt(3))
-            for x in np.arange(-np.ceil(2.*R/a1), np.ceil(2.*R/a1)):
-                for y in np.arange(-np.ceil(2.*R/a1), np.ceil(2.*R/a1)):
-                    if looptype == 'int':
-                        if crystaltype == 'FCC':
-                            r = x*ex_p+y*ey_p
-                        elif crystaltype == 'BCC':
-                            r = (x-0.25)*ex_p+(y-0.25)*ey_p
-
-                        if np.linalg.norm(r) <= R:
+            for x_p in np.arange(-np.ceil(2.*R/a1), np.ceil(2.*R/a1)):
+                for y_p in np.arange(-np.ceil(2.*R/a1), np.ceil(2.*R/a1)):
+                    r = x_p*ex_p+y_p*ey_p
+                    if np.linalg.norm(r) <= R:
+                        if looptype == 'int':
                             amplitude += np.exp(\
                                 -0.5*r.dot(r)/D**2)*np.cos(qloop.dot(r))
-                    elif looptype == 'vac':
-                        ''' According to the configuration defined in the 
-                                parameter file, the z=0 plane should be type C for FCC,  
-                                and type A for BCC
-                        '''
-                        if crystaltype == 'FCC':
-                            r = (x+orig_C[0])*ex_p+(y+orig_C[1])*ey_p
-                        elif crystaltype == 'BCC':
-                            r = x*ex_p+y*ey_p
-                        if np.linalg.norm(r) <= R:
+                        elif looptype == 'vac':
                             amplitude -= np.exp(\
                                 -0.5*r.dot(r)/D**2)*np.cos(qloop.dot(r))
             amplitudes.append([qval, amplitude])
 
         amplitudes = np.array(amplitudes)
-        amplitudes1 = np.copy(amplitudes)  # amplitude inside rho<5R, |z|<10R for comparison
-        amplitudes2 = np.copy(amplitudes)  # amplitude inside rho<10R, |z|<20R for comparison
+        #amplitudes1 = np.copy(amplitudes)  # amplitude inside rho<5R, |z|<10R for comparison
+        #amplitudes2 = np.copy(amplitudes)  # amplitude inside rho<10R, |z|<20R for comparison
+        ff_tier2 = np.array(ff_tier2)
+        ff_tier3 = np.array(ff_tier3)
 
-        i_file = 0 
-        while os.path.isfile(datadir+"%s_atoms_amplitude_%s_R%d_ori0_%04d.npy"%(\
-            sample, looptype, R, i_file)):
-            amplitudes[:,1] += np.load(datadir+"%s_atoms_amplitude_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file))[:,1]
-            amplitudes1[:,1] += np.load(datadir+"%s_atoms_amplitude1_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file))[:,1]
-            amplitudes2[:,1] += np.load(datadir+"%s_atoms_amplitude2_%s_R%d_ori%d_%04d.npy"%(\
-                                sample, looptype, R, i_ori, i_file))[:,1]
-            i_file += 1
+        amplitudes[:,1] += np.load(datadir+"%s_atoms_amplitude_%s_T1_R%d_ori%d_combined.npy"%(\
+                            sample, looptype, R, i_ori))[:,1]
+        amplitudes[:,1] += np.load(datadir+"%s_atoms_amplitude_%s_T2_R%d_ori%d_combined.npy"%(\
+                            sample, looptype, R, i_ori))[:,1]*ff_tier2
+        amplitudes[:,1] += np.load(datadir+"%s_atoms_amplitude_%s_T3_R%d_ori%d_combined.npy"%(\
+                            sample, looptype, R, i_ori))[:,1]*ff_tier3
 
         intensities.append(amplitudes[:,1]**2)
-        intensities1.append(amplitudes1[:,1]**2)
-        intensities2.append(amplitudes2[:,1]**2)
+        #intensities1.append(amplitudes1[:,1]**2)
+        #intensities2.append(amplitudes2[:,1]**2)
         print "read %d files, done orientation %d" % (i_file, i_ori)
 
     # average over all orientations
     intensities = np.mean(np.array(intensities), axis=0)
-    intensities1 = np.mean(np.array(intensities1), axis=0)
-    intensities2 = np.mean(np.array(intensities2), axis=0)
+    #intensities1 = np.mean(np.array(intensities1), axis=0)
+    #intensities2 = np.mean(np.array(intensities2), axis=0)
 
     # plot q^4/R^2*I 
     if looptype == 'vac':
@@ -87,8 +78,8 @@ for looptype in looptypes:
     elif looptype == 'int':
         linestyle = '-'
     ax.plot(q_array, intensities*(q_array)**4/R**2, color='r', ls=linestyle)
-    ax.plot(q_array, intensities1*(q_array)**4/R**2, color='b', ls=linestyle)
-    ax.plot(q_array, intensities2*(q_array)**4/R**2, color='g', ls=linestyle)
+    #ax.plot(q_array, intensities1*(q_array)**4/R**2, color='b', ls=linestyle)
+    #ax.plot(q_array, intensities2*(q_array)**4/R**2, color='g', ls=linestyle)
 
 ax.set_xlim(-0.5, 0.5)
 ax.set_ylim(0., 10.)
