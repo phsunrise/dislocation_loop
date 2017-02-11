46decb20d2e094bd986b89ff3cd460e701e31b1b
Peihao Sun <phsun@sherlock-ln02.stanford.edu>
2017-02-11 14:24:29 -0800

    minor changes

--- s_iso.py
+++ s_iso.py
@@ -1,21 +1,30 @@
-'''
-Adaptive quadrature method
-'''
 import numpy as np
 from scipy.integrate import dblquad 
 import sys, os
 from datetime import datetime
 from getopt import getopt
-from info import MAXTIER, NFILES
-from settings import basedir, preproc_dir 
+import pickle
+from settings import MAXTIER, NFILES, basedir, preproc_dir 
 from displacement_iso import disp 
 
 do_debug = False
+do_mpi = True
 
-opts, args = getopt(sys.argv[1:], "d")
+opts, args = getopt(sys.argv[1:], "dn:p:")
 for o, a in opts:
     if o == '-d':
         do_debug = True
+    elif o == '-n':
+        nprocs = int(a)
+        do_mpi = False
+    elif o == '-p':
+        rank = int(a)
+
+if do_mpi:
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    nprocs = comm.Get_size()
 
 sample = 'W'
 looptypes = ['vac', 'int']
@@ -32,22 +41,22 @@ if not os.path.isdir(datadir):
     os.system("mkdir %s"%datadir)
 
 # get uncalculated files
-filelist = []
-for looptype in looptypes:
-    for tier in range(1, MAXTIER+1):
-        for i_file in xrange(NFILES):
-            if os.path.isfile(preproc_dir+"%s_atoms_s_%s_pre_T%d_%04d.npy"%(\
-               sample, looptype, tier, i_file)) and not os.path.isfile(datadir+"%s_atoms_s_%s_T%d_R%d_%04d.npy"%(sample, looptype, tier, R, i_file)):
-                filelist.append([looptype, tier, i_file])
 if do_debug:
+    filelist = []
+    for looptype in looptypes:
+        for tier in range(1, MAXTIER+1):
+            for i_file in xrange(NFILES):
+                if os.path.isfile(preproc_dir+"%s_atoms_s_%s_pre_T%d_%04d.npy"%(\
+                   sample, looptype, tier, i_file)) and not os.path.isfile(datadir+"%s_atoms_s_%s_T%d_R%d_%04d.npy"%(sample, looptype, tier, R, i_file)):
+                    filelist.append([looptype, tier, i_file])
     print filelist
     print "%d files" % len(filelist)
+    with open("s_iso_filelist.pickle", 'wb') as fout:
+        pickle.dump(filelist, fout)
     sys.exit(0)
+else:
+    filelist = pickle.load(open("s_iso_filelist.pickle", 'rb'))
 
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-nprocs = comm.Get_size()
 for i_i_file, [looptype, tier, i_file] in enumerate(filelist):
     if i_i_file % nprocs != rank:
         continue
@@ -97,10 +106,7 @@ for i_i_file, [looptype, tier, i_file] in enumerate(filelist):
         #print "time elapsed:", time_2-time_1
 
         s = disp(Bloop, x, y, z, 1., C12, C44)
-        if tier == 1:
-            data.append([xyz[0], xyz[1], xyz[2], s[0], s[1], s[2], xyz[3]])
-        else:
-            data.append([xyz[0], xyz[1], xyz[2], s[0], s[1], s[2]])
+        data.append([xyz[0], xyz[1], xyz[2], s[0], s[1], s[2]])
         
         #if i_xyz % 10 == 0:
         #    print "done %s, tier %d, file %04d, entry %04d, at %s" % (\
